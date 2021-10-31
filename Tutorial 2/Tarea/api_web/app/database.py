8b2d1458c8d31c803d5f179fea355323bdf09caf
Nicolás Robledo <nrobledosagredo@gmail.com>
2021-10-31 02:05:00 -0300

    tarea 2

--- Tutorial 2/Tarea/api_web/app/database.py
+++ Tutorial 2/Tarea/api_web/app/database.py
@@ -1,8 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://my-api:my-api-password@127.0.0.1:3306/tutorial1"
+DATABASE_IP = str(os.environ['DATABASE_IP'])
+
+DATABASE_USER = "root"
+DATABASE_PASS = "root"
+
+DATABASE = 'tutorial_1'
+PORT = 3306
+SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{DATABASE_USER}:{DATABASE_PASS}@{DATABASE_IP}:{PORT}/{DATABASE}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
---
+++ Tutorial 2/Tarea/database/create_database/create_database.py
@@ -0,0 +1,39 @@
+import mysql.connector
+import os, time
+
+def create_database(db_connection,db_name,cursor):
+	cursor.execute(f"CREATE DATABASE {db_name};")
+	cursor.execute(f"COMMIT;")
+	cursor.execute(f"USE {db_name};")
+
+
+#######################
+DATABASE = "tutorial_1"
+
+DATABASE_IP = str(os.environ['DATABASE_IP'])
+
+DATABASE_USER = "root"
+DATABASE_USER_PASSWORD = "root"
+DATABASE_PORT=3306
+
+not_connected = True
+
+while(not_connected):
+	try:
+		print(DATABASE_IP,"IP")
+		db_connection = mysql.connector.connect(user=DATABASE_USER,host=DATABASE_IP,port=DATABASE_PORT, password=DATABASE_USER_PASSWORD)
+		not_connected = False
+
+	except Exception as e:
+		time.sleep(3)
+		print(e, "error!!!")
+		print("can't connect to mysql server, might be intializing")
+		
+cursor = db_connection.cursor()
+
+try:
+	cursor.execute(f"USE {DATABASE}")
+	print(f"Database: {DATABASE} already exists")
+except Exception as e:
+    create_database(db_connection,DATABASE,cursor)
+    print(f"Succesfully created: {DATABASE}")
