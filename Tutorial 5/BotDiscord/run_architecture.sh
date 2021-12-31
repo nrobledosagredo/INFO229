45b351a6ec3b415e181c0bf19c44e9d186785b11
Nicolás Robledo <nrobledosagredo@gmail.com>
2021-12-31 20:52:59 -0300

    funcionalidad de youtube con el bot

--- Tutorial 5/BotDiscord/run_architecture.sh
+++ Tutorial 5/BotDiscord/run_architecture.sh
@@ -1,4 +1,7 @@
+docker-compose -f docker-compose.yml up -d --build rabbitmq
+
 docker-compose -f docker-compose.yml up -d --build birthday_database
+
 
 DATABASE_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' birthday_database) 
 
@@ -7,3 +10,5 @@ echo $DATABASE_IP
 DATABASE_IP=$DATABASE_IP docker-compose -f docker-compose.yml up -d --build birthday_manager
 
 docker-compose -f docker-compose.yml up -d --build discord_listener
+
+docker-compose -f docker-compose.yml up -d --build youtube
